9f32ca4116dc1a3d4185579273f557064c5f1c04
feefifofannie <thedanielleredmond@gmail.com>
2015-09-23 16:10:46 +0530

    registration validation

---
+++ FashionStreet/src/com/sapient/controller/RegistrationServlet.java
@@ -0,0 +1,57 @@
+package com.sapient.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class RegistrationServlet
+ */
+public class RegistrationServlet extends HttpServlet {
+	List<String> emails = new ArrayList<String>();
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistrationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		emails.add("dlredmond@gatech.edu");
+		PrintWriter out = response.getWriter();
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		for (String existing:emails){
+			if (email.equals(existing)){
+				   out.println("<script type=\"text/javascript\">");
+				   out.println("alert('This email is already registered with Fashion Street.');");
+				   out.println("location='index.jsp';");
+				   out.println("</script>");   
+			}
+		}
+	}
+
+}
